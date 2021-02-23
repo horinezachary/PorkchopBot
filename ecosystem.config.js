eb0e554cb8b83ecfc56a6e49a50609625e225d5e
ZacharyHorine <zachary@horine.dev>
2021-02-22 22:15:10 -0800

    increment version number

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,6 @@ module.exports = {
         name: "PorkchopBot",
         script: "npm start",
         watch: true,
-	ignore_watch : ["db"],
         env: {
           "NODE_ENV": "production",
 	        "NODE_PATH": "/root/.nvm/versions/node/v15.2.1/bin/node"
