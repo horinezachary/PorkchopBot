43db3bd804b1bdfca2f27e98931ba99fe5ca15e2
ZacharyHorine <zachary@horine.dev>
2021-02-28 13:27:39 -0800

    update guildCreate for economy

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -6,6 +6,6 @@ module.exports = class {
   }
 
   async run(bot,guild) {
-    await bot.database.query(`INSERT IGNORE INTO guild(guild_id,prefix) VALUES("${guild.id}","${bot.config.PREFIX}")`);
+    await bot.database.query(`INSERT IGNORE INTO guild(guild_id,prefix,guild_economy_mode,guild_economy_daily) VALUES("${guild.id}","${bot.config.PREFIX}","GLOBAL",${bot.config.economy.GLOBAL_DAILY})`);
   }
 }
